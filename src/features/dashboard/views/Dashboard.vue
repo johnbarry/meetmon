f2225f7803a6dc9819d4f74befecaeca97f8c666
Daniel Beckmann <danielbeckmann@live.de>
2022-06-09 17:39:52 +0200

    Added dashboard view

---
+++ src/features/dashboard/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<script>
+import LeaderboardTable from '../components/LeaderboardTable.vue';
+import PersonalScore from '../components/PersonalScore.vue';
+
+export default {
+  components: {
+    LeaderboardTable,
+    PersonalScore
+  }
+};
+</script>
+
+<template>
+  <h1>Meeting Rating</h1>
+  <LeaderboardTable></LeaderboardTable>
+  <PersonalScore></PersonalScore>
+</template>
+
+<style scoped>
+
+</style>
