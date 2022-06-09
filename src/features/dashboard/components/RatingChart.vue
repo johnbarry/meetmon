0b2dd0b4a73bd7eaa740923088dd1da2ea523471
Daniel Beckmann <danielbeckmann@live.de>
2022-06-09 18:13:35 +0200

    migrated ranking chart to a feature of dashboard

--- src/features/dashboard/components/RatingChart.vue
+++ src/features/dashboard/components/RatingChart.vue
@@ -1,3 +1,31 @@
+<script>
+import Chart from 'chart.js'
+import rankingChartData from '../data/ranking-data';
+
+export default {
+  name: 'RankingChart',
+  data() {
+    return {
+      rankingChartData: rankingChartData
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('ranking-chart');
+    new Chart(ctx, this.rankingChartData);
+  }
+}
+</script>
+
 <template>
   <h2>Your Rating over Time</h2>
+  <div class="container">
+    <canvas id="ranking-chart"></canvas>
+  </div>
 </template>
+
+
+<style scoped>
+.container {
+  max-width: 600px;
+}
+</style>
