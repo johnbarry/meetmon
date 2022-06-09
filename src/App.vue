11249060c987b93aaaf3b36d9f41aa69a34c9b30
johnbarry <john.barry@me.com>
2022-06-09 08:58:38 -0400

    dont underline if all agenda items are level 1

--- src/App.vue
+++ src/App.vue
@@ -10,43 +10,51 @@ export default {
         item.active = item.number == number;
       }
     },
-    updateItemScore: function(item, score) {
-      item.scoreCount++
-      item.scoreTotal += score
+    updateItemScore: function (item, score) {
+      item.scoreCount++;
+      item.scoreTotal += score;
     },
     averageScoreSize: function (avg) {
-      if (avg > 3) return 'L'
-      if (avg >= 1.5) return 'M'
-      if (avg >= 1) return 'S'
-      return '0'
+      if (avg > 3) return "L";
+      if (avg >= 1.5) return "M";
+      if (avg >= 1) return "S";
+      return "0";
     },
     averageItemScore: function (item) {
-      return this.averageScoreSize(item.scoreTotal / item.scoreCount)
+      return this.averageScoreSize(item.scoreTotal / item.scoreCount);
     },
-    sizeToColorMap: function() {
+    sizeToColorMap: function () {
       return {
-        L: 'green',
-        M: 'lightgreen',
-        S: 'orange',
-        '0': 'red',
-        '': 'lightgrey'
-      }
+        L: "green",
+        M: "lightgreen",
+        S: "orange",
+        0: "red",
+        "": "lightgrey",
+      };
     },
     itemScoreStyle: function (item) {
-      return  `background-color: ${  this.sizeToColorMap()[this.averageItemScore(item)] }`
+      return `background-color: ${this.sizeToColorMap()[this.averageItemScore(item)]}`;
     },
     averageMeetingColor: function (agendaItems) {
       let [totalItems, totalMinutes, totalWeightedScore] = agendaItems.reduce(
-        (totals, item) => (!item.minutes || item.scoreCount == 0) ? totals : [ 
-          totals[0]+1, // number of items
-          totals[1] += item.minutes, // total minutes
-          totals[2] += (item.scoreTotal * item.minutes) / item.scoreCount
-          ]  ,
-        [0, 0, 0.0])
-        console.log(`[totalItems, totalMinutes, totalWeightedScore]=[${totalItems} ${totalMinutes} ${totalWeightedScore}]`)
-        let color = this.sizeToColorMap()[totalItems == 0 ? '' : this.averageScoreSize(  totalWeightedScore / totalMinutes)]
-        console.log(`color is ${color}`)
-        return color
+        (totals, item) =>
+          !item.minutes || item.scoreCount == 0
+            ? totals
+            : [
+                totals[0] + 1, // number of items
+                (totals[1] += item.minutes), // total minutes
+                (totals[2] += (item.scoreTotal * item.minutes) / item.scoreCount),
+              ],
+        [0, 0, 0.0]
+      );
+      console.log(
+        `[totalItems, totalMinutes, totalWeightedScore]=[${totalItems} ${totalMinutes} ${totalWeightedScore}]`
+      );
+      let color = this.sizeToColorMap()[
+        totalItems == 0 ? "" : this.averageScoreSize(totalWeightedScore / totalMinutes)
+      ];
+      console.log(`color is ${color}`);
+      return color;
     },
   },
   data() {
@@ -60,9 +68,9 @@ export default {
             level: level,
             active: t.active,
             minutes: t.minutes,
-	    notes: t.notes,
+            notes: t.notes,
             scoreCount: 0,
-            scoreTotal: 0
+            scoreTotal: 0,
           };
 
         if (t.topics) yield* items(t.topics, level + 1, newNumber);
@@ -70,46 +78,46 @@ export default {
     }
     let urlParams = new URLSearchParams(window.location.search);
     let raw = {
-      defaultMeeting: urlParams.has('id') ?  urlParams.get('id') : 1,
+      defaultMeeting: urlParams.has("id") ? urlParams.get("id") : 1,
       meetings: {
         2: {
-	   id: 2,
-	   time: new Date(),
-	   title: "Kickoff",
-	   topics: [
-	   	{
-	     	name: "Change current meeting page - John, Anil, Syed",
-		minutes: 5,
-	     	notes: [
-			"stepping thru agenda",
-			"entering meeting minutes",
-			"algo discussion"
-	     	]
-	     },
-	   	{
-	     	name: "Design/implmenent ratings page - Daniel, Gaurav, Naveen",
-             	active: true,
-		minutes: 5,
-	     	notes: [
-			"focus on UI, wireframes",
-			"simple, ordered list",
-			"averages not single meeting"
-	     	]
-	     },
-	   	{
-	     	name: "Teams integration - Syed, Daniel",
-		minutes: 5,
-	     	notes: [ "ON HOLD" ]
-	     },
-	   	{
-	     	name: "Objective",
-		minutes: 1,
-	     	notes: [
-			"Provide chair rating to enable participants to choose effective meetings and a means for chairs to improve"
-			]
-	     }
-	   ]
-	},
+          id: 2,
+          time: new Date(),
+          title: "Kickoff",
+          topics: [
+            {
+              name: "Change current meeting page - John, Anil, Syed",
+              minutes: 5,
+              notes: [
+                "stepping thru agenda",
+                "entering meeting minutes",
+                "algo discussion",
+              ],
+            },
+            {
+              name: "Design/implmenent ratings page - Daniel, Gaurav, Naveen",
+              active: true,
+              minutes: 5,
+              notes: [
+                "focus on UI, wireframes",
+                "simple, ordered list",
+                "averages not single meeting",
+              ],
+            },
+            {
+              name: "Teams integration - Syed, Daniel",
+              minutes: 5,
+              notes: ["ON HOLD"],
+            },
+            {
+              name: "Objective",
+              minutes: 1,
+              notes: [
+                "Provide chair rating to enable participants to choose effective meetings and a means for chairs to improve",
+              ],
+            },
+          ],
+        },
         1: {
           id: 1,
           time: new Date(),
@@ -125,10 +133,10 @@ export default {
                   topics: [
                     {
                       name: "Button color changes",
-		      notes: [
-		      	"Harry said it was super cool",
-			"Nadia wasn't so impressed"
-		      ]
+                      notes: [
+                        "Harry said it was super cool",
+                        "Nadia wasn't so impressed",
+                      ],
                     },
                     {
                       name: "Save button",
@@ -148,27 +156,24 @@ export default {
               topics: [
                 {
                   name: "Big bang approach: Pros & cons",
-		  notes: [
-		    "Pro: Simple to implement",
-		    "Con: High release failure risk",
-		  ]
+                  notes: ["Pro: Simple to implement", "Con: High release failure risk"],
                 },
                 {
                   name: "Incremental change: Pros & Cons",
-		  notes: [
-		    "Con: Need feature switch for new/old database approach",
-		    "Con: Twice as many test cases",
-		    "Pro: Easier to revert",
-		    "Pro: Less likely to get fired"
-		  ]
+                  notes: [
+                    "Con: Need feature switch for new/old database approach",
+                    "Con: Twice as many test cases",
+                    "Pro: Easier to revert",
+                    "Pro: Less likely to get fired",
+                  ],
                 },
                 {
                   name: "Propose big bang or incremental",
-		  notes: [
-		    "Decided on incremental approach",
-		    "Joe to produce plan for next week's meeting",
-		    "Getting fired is bad"
-		  ]
+                  notes: [
+                    "Decided on incremental approach",
+                    "Joe to produce plan for next week's meeting",
+                    "Getting fired is bad",
+                  ],
                 },
               ],
             },
@@ -176,28 +181,30 @@ export default {
         },
       },
     };
-    raw.agendaItems = Array.from(items(raw.meetings[raw.defaultMeeting].topics))
-    return raw
+    raw.agendaItems = Array.from(items(raw.meetings[raw.defaultMeeting].topics));
+    raw.justLevelOne = raw.agendaItems.reduce( (levelOneSoFar, item) => levelOneSoFar && item.level == 1 )
+    return raw;
   },
 };
 </script>
 
 <template>
-  
-  <h1>{{ meetings[1].title }} ({{ meetings[1].time.toLocaleString() }}) 
-  <button v-bind:style="`background-color: ${averageMeetingColor(agendaItems)}`">score</button>
-      
+  <h1>
+    {{ meetings[1].title }} ({{ meetings[1].time.toLocaleString() }})
+    <button v-bind:style="`background-color: ${averageMeetingColor(agendaItems)}`">
+      score
+    </button>
   </h1>
   <table>
-  <thead>
-  	<tr>
-		<th>Item</th>
-		<th>Time</th>
-		<th>Subject</th>
-		<th>Minutes</th>
-		<th colspan="5">Score</th>
-	</tr>
-  </thead>
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Time</th>
+        <th>Subject</th>
+        <th>Minutes</th>
+        <th colspan="5">Score</th>
+      </tr>
+    </thead>
     <tbody>
       <tr v-for="item in agendaItems" :key="item.number" :ref="`agenda:${item.number}`">
         <td
@@ -208,15 +215,17 @@ export default {
             })
           "
           @click="setActive(item.number)"
-        > {{ item.number }}
+        >
+          {{ item.number }}
         </td>
-        <td 
-          v-if="item.level == 1" 
+        <td
+          v-if="item.level == 1"
           v-bind:style="maybeHighlight(item, {})"
           @click="setActive(item.number)"
-        > {{ item.minutes}} mins
+        >
+          {{ item.minutes }} mins
         </td>
-        <td 
+        <td
           v-else
           v-bind:style="maybeHighlight(item, {})"
           @click="setActive(item.number)"
@@ -225,64 +234,60 @@ export default {
           v-bind:style="
             maybeHighlight(
               item,
-              item.level == 1 ? { 'text-decoration': 'underline' } : {}
+              item.level == 1 && !justLevelOne ? { 'text-decoration': 'underline' } : {}
             )
           "
           @click="setActive(item.number)"
         >
-        <span v-if="item.level == 1">{{ item.title }}</span>
-        <span v-else>{{ `${'  '.repeat(item.level - 2)}- ${item.title}` }}</span>
+          <span v-if="item.level == 1">{{ item.title }}</span>
+          <span v-else>{{ `${"  ".repeat(item.level - 2)}- ${item.title}` }}</span>
         </td>
 
         <td
-          v-bind:style="
-            maybeHighlight(
-              item,
-              { 'font-size': 'small' } 
-            )
-          "
+          v-bind:style="maybeHighlight(item, { 'font-size': 'small' })"
           @click="setActive(item.number)"
         >
-	<ol>
-	<li v-for="n in item.notes">{{ n }} </li>
-	</ol></td>
+          <ol>
+            <li v-for="n in item.notes">{{ n }}</li>
+          </ol>
+        </td>
 
-        <td v-if="item.level == 1"
-          v-bind:style=" maybeHighlight( item, {}) "
-         @click="updateItemScore(item,0)"
+        <td
+          v-if="item.level == 1"
+          v-bind:style="maybeHighlight(item, {})"
+          @click="updateItemScore(item, 0)"
         >
           <button style="background-color: red">0</button>
         </td>
-        <td 
+        <td
           v-if="item.level == 1"
-          v-bind:style=" maybeHighlight( item, {}) "
-          @click="updateItemScore(item,1)"
-          >
+          v-bind:style="maybeHighlight(item, {})"
+          @click="updateItemScore(item, 1)"
+        >
           <button style="background-color: orange">S</button>
         </td>
-        <td 
+        <td
           v-if="item.level == 1"
-          v-bind:style=" maybeHighlight( item, {}) "
-         @click="updateItemScore(item,2)"
-          >
+          v-bind:style="maybeHighlight(item, {})"
+          @click="updateItemScore(item, 2)"
+        >
           <button style="background-color: lightgreen">M</button>
         </td>
-        <td 
+        <td
           v-if="item.level == 1"
-          v-bind:style=" maybeHighlight( item, {}) "
-          @click="updateItemScore(item,4)"
-          >
+          v-bind:style="maybeHighlight(item, {})"
+          @click="updateItemScore(item, 4)"
+        >
           <button style="background-color: green">L</button>
         </td>
-        <td 
-	  v-if="item.level == 1 && item.scoreCount > 0"
-          v-bind:style=" maybeHighlight( item, {}) ">
-        <span style="font-size: small"> x{{item.scoreCount}}
-        </span> = 
+        <td
+          v-if="item.level == 1 && item.scoreCount > 0"
+          v-bind:style="maybeHighlight(item, {})"
+        >
+          <span style="font-size: small"> x{{ item.scoreCount }} </span> =
           <button v-bind:style="itemScoreStyle(item)">
             {{ averageItemScore(item) }}
-           </button> 
-           
+          </button>
         </td>
       </tr>
     </tbody>
